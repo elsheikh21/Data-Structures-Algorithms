ac50df50e2c266e4033a4fec65fc85926e208ea3
El Sheikh <ahmedhelsheikh@gmail.com>
2018-01-05 03:03:43 +0200

    Halfway through stacks

--- src/sheikh/stacks/ArrayStack.java
+++ src/sheikh/stacks/ArrayStack.java
@@ -4,22 +4,22 @@ import sheikh.utils.Utilities;
 
 public class ArrayStack {
 	private int size;
-	private char[] arrayStack;
+	private int[] arrayStack;
 	private int top;
 
 	public int getSize() {
-		return size;
+		return top + 1;
 	}
 
 	public void setSize(int size) {
 		this.size = size;
 	}
 
-	public char[] getArrayStack() {
+	public int[] getArrayStack() {
 		return arrayStack;
 	}
 
-	public void setArrayStack(char[] arrayStack) {
+	public void setArrayStack(int[] arrayStack) {
 		this.arrayStack = arrayStack;
 	}
 
@@ -37,17 +37,17 @@ public class ArrayStack {
 
 	public ArrayStack(int maxSize) {
 		size = maxSize;
-		arrayStack = new char[size];
+		arrayStack = new int[size];
 		top = -1;
 	}
 
-	public void push(char x) {
+	public void push(int i) {
 		top++;
-		arrayStack[top] = x;
+		arrayStack[top] = i;
 	}
 
-	public char pop() {
-		char result = arrayStack[top];
+	public int pop() {
+		int result = arrayStack[top];
 		top--;
 		return result;
 	}
@@ -64,12 +64,62 @@ public class ArrayStack {
 		return top == size - 1;
 	}
 
-	public static void main(String[]args) {
-		String word = "(([()[]][])[])";
+	public void printStack() {
+		if (top == -1) {
+			System.out.println("The array is empty");
+		}
+		System.out.print("Stack elements are: ");
+		for (int i = 0; i < arrayStack.length; i++)
+			System.out.print(arrayStack[i] + " ");
+		System.out.println();
+	}
+
+	public int searchInStack(int number) {
+		int index = 0;
+		int atIndex = -1;
+		for (int i = 0; i < arrayStack.length; i++) {
+			if (arrayStack[i] != number) {
+				index++;
+			} else {
+				atIndex = index++;
+			}
+		}
+		return atIndex;
+	}
+
+	public static void main(String[] args) {
+		ArrayStack arrayStack = new ArrayStack(10);
+		arrayStack.push(6);
+		arrayStack.push(9);
+		arrayStack.push(3);
+		arrayStack.push(5);
+		arrayStack.push(4);
+		arrayStack.push(1);
+		arrayStack.push(2);
+		arrayStack.push(7);
+		arrayStack.push(72);
+		arrayStack.push(17);
+
 		Utilities utilities = new Utilities();
-		if(utilities.checkParentheses(word))
-			System.out.println(String.format("This word '%s' is matching Parentheses", word));
-		else 
-			System.out.println(String.format("This word '%s' is not a matching Parentheses", word));
+		
+//		String word = "(([()[]][])[])";
+//		String print = utilities.checkParentheses(word)
+//				? String.format("This word '%s' is matching Parentheses", word)
+//				: String.format("This word '%s' is not a matching Parentheses", word);
+//		System.out.println(print);
+//
+//		int atIndex = utilities.searchInStack(arrayStack, 3);
+//		print = (atIndex == -1) ? "The number you are searching for is not found"
+//				: String.format("The number is found at index of (%1$d)", atIndex);
+//		System.out.println(print);
+//
+//		atIndex = arrayStack.searchInStack(6);
+//		print = (atIndex == -1) ? "The number you are searching for is not found"
+//				: String.format("The number is found at index of (%1$d)", atIndex);
+//		System.out.println(print);
+//		
+//		Utilities.decompose(arrayStack);
+//		
+//		utilities.sort(arrayStack).printStack();
 	}
 }
